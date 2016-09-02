86a252ef45248bc3f64530eae528d37931a606be
lucycy <lucy.permatasari@reebonz.com>
2016-09-02 17:48:37 +0700

    fix slider for iOS 9 and iOS 8 difference

--- UISliderCustom/UISliderCustom.swift
+++ UISliderCustom/UISliderCustom.swift
@@ -8,17 +8,24 @@
 
 import UIKit
 
+let COLOR_OPTION_BG = UIColor(red: 250.0/255.0, green: 250.0/255.0, blue: 250.0/255.0, alpha: 1.0)
+let COLOR_OPTION_TEXT = UIColor(red: 74.0/255.0, green: 74.0/255.0, blue: 74.0/255.0, alpha: 1.0)
+let COLOR_DUMMY = UIColor(red: 174.0/255.0, green: 94.0/255.0, blue: 24.0/255.0, alpha: 5.0)
+
+let IOS_VERSION: CGFloat = CGFloat((UIDevice.currentDevice().systemVersion as NSString).floatValue)
+
 protocol DelegateUISliderCustom: NSObjectProtocol {
   func valueDidChanged(value: Float)
 }
 
 class UISliderCustom: UISlider {
-  
+
   lazy var vwTrackLeft: UIView = UIView()
   lazy var vwTrackRight: UIView = UIView()
   
   lazy var vwMaskLeft: UIView = UIView()
   lazy var vwMaskRight: UIView = UIView()
+  lazy var vwDummy: UIView = UIView()
   lazy var layerMaskRight: CALayer = CALayer()
   lazy var layerMaskLeft: CALayer = CALayer()
   
@@ -43,22 +50,25 @@ class UISliderCustom: UISlider {
   
   override func awakeFromNib() {
     super.awakeFromNib()
-    self.vwTrackLeft.backgroundColor = UIColor.redColor()
-    self.vwTrackRight.backgroundColor = UIColor.blueColor()
+    self.vwTrackLeft.backgroundColor = COLOR_OPTION_TEXT
+    self.vwTrackRight.backgroundColor = COLOR_OPTION_TEXT
     
-    self.vwMaskRight.backgroundColor = colorUnfilled
-    self.vwMaskLeft.backgroundColor = colorUnfilled
+    self.vwMaskRight.backgroundColor = UIColor.redColor() //colorUnfilled
+    self.vwMaskLeft.backgroundColor = UIColor.blueColor() //colorUnfilled
+    //self.vwDummy.backgroundColor = COLOR_DUMMY
     self.layerMaskLeft.backgroundColor = UIColor.blackColor().CGColor
     self.layerMaskRight.backgroundColor = UIColor.blackColor().CGColor
     
-    self.minimumValue = -100
-    self.maximumValue = 100
-    
+    self.thumbTintColor = COLOR_OPTION_TEXT
+    if IOS_VERSION < 9 {
+       self.setThumbImage(getImageWithColor(COLOR_OPTION_TEXT, size: CGSizeMake(20, 20)), forState: UIControlState.Normal)
+    }
+   
     self.addSubview(self.vwTrackLeft)
     self.addSubview(self.vwTrackRight)
     self.addSubview(self.vwMaskLeft)
     self.addSubview(self.vwMaskRight)
-    
+    self.addSubview(self.vwDummy)
     self.setMinimumTrackImage(UIImage(), forState: UIControlState.Normal)
     self.setMaximumTrackImage(UIImage(), forState: UIControlState.Normal)
     self.continuous = true
@@ -71,8 +81,13 @@ class UISliderCustom: UISlider {
     self.lblIndicator.font = UIFont.systemFontOfSize(11)
     self.lblIndicator.hidden = true
     self.lblIndicator.textAlignment = NSTextAlignment.Center
-    //self.lblIndicator.backgroundColor = UIColor.redColor()
+    
     self.addSubview(self.lblIndicator)
+    
+    self.minimumValue = -0.5
+    self.maximumValue = 0.5
+    self.value = 0
+    self.floatDefaultValue = 0
     
     
   }
@@ -91,6 +106,7 @@ class UISliderCustom: UISlider {
     self.vwMaskRight.frame = self.vwTrackRight.frame
     
     //debugPrint("layout subviews")
+    
   }
   
   func hideLabelIndicator() {
@@ -98,7 +114,7 @@ class UISliderCustom: UISlider {
     if self.value > (self.floatDefaultValue - 0.05) && self.value < (self.floatDefaultValue + 0.05) && self.value != floatDefaultValue {
       self.value = self.floatDefaultValue
     }
-    
+   
     self.lblIndicator.hidden = true
   }
   
@@ -127,14 +143,17 @@ class UISliderCustom: UISlider {
     
     self.rectThumb = self.thumbRectForBounds(self.bounds, trackRect: self.frame, value: self.value)
     
+    //self.vwDummy.frame = rectThumb
+    
     self.lblIndicator.hidden = false
     var point: CGPoint = self.lblIndicator.center
-    point.x = self.rectThumb.origin.x - 20
+    point.x = self.rectThumb.origin.x 
     
     self.lblIndicator.center = point
     
     let floatx: CGFloat = self.rectThumb.origin.x - self.frame.origin.x + floatGap
-    
+    debugPrint(floatx)
+  
     if floatx > self.vwTrackLeft.frame.size.width {
       var rect: CGRect = self.layerMaskLeft.frame
       rect.size.width = self.vwTrackLeft.frame.size.width
@@ -144,6 +163,9 @@ class UISliderCustom: UISlider {
       rect.origin.x = floatx - self.vwMaskRight.frame.size.width
       self.layerMaskRight.frame = rect
       
+      //debugPrint("MASK LEFT", self.layerMaskLeft.frame)
+      //debugPrint("MASK RIGHT", self.layerMaskRight.frame)
+  
     } else {
       
       var rect: CGRect = self.layerMaskRight.frame
@@ -153,16 +175,37 @@ class UISliderCustom: UISlider {
       rect = self.layerMaskLeft.frame
       rect.size.width = floatx
       self.layerMaskLeft.frame = rect
+      
+       //debugPrint("MASK LEFT", self.layerMaskLeft.frame)
+       //debugPrint("MASK RIGHT", self.layerMaskRight.frame)
     }
     
-    self.lblIndicator.text = String(format: "%d", Int(self.value))
     self.delUISlidercustom?.valueDidChanged(self.value)
+    
+    self.lblIndicator.text = String(format:"%d",Int(self.value))
   }
+  
+  func getImageWithColor(color: UIColor, size: CGSize) -> UIImage {
+   
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(size.width , size.height), false, 0)
+    let ctx = UIGraphicsGetCurrentContext()
+    CGContextSaveGState(ctx)
+    
+    let rect = CGRectMake(0, 0, size.width, size.height)
+    CGContextSetFillColorWithColor(ctx, color.CGColor)
+    CGContextFillEllipseInRect(ctx, rect)
+    
+    CGContextRestoreGState(ctx)
+    let img = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return img
+  }
+
   //
   
   /* for custom its height
-   override func trackRectForBounds(bounds: CGRect) -> CGRect {
-   return CGRectMake(0, 0, 100, 10)
-   }
-   */
+ override func trackRectForBounds(bounds: CGRect) -> CGRect {
+    return CGRectMake(0, 0, 100, 10)
+ }
+ */
 }
